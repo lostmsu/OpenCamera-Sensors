1330921d072f210acc24fef4febfdb24de9d5b41
Victor Nova <lostfreeman@gmail.com>
2026-03-15 15:08:24 -0400

    use user-preferred orientation in landscape mode

--- app/src/main/java/net/sourceforge/opencamera/MainActivity.java
+++ app/src/main/java/net/sourceforge/opencamera/MainActivity.java
@@ -3183,7 +3183,7 @@ public class MainActivity extends Activity {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         // force to landscape mode
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE);
         //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE); // testing for devices with unusual sensor orientation (e.g., Nexus 5X)
         if( preview != null ) {
             // also need to call setCameraDisplayOrientation, as this handles if the user switched from portrait to reverse landscape whilst in settings/etc
